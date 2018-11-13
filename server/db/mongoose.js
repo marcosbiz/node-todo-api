f3e5bd55958b9a7a127ca9be858caf3609d161cb
Marcos Silva <marcosfp@Marks-MacBook.local>
2018-11-12 22:33:56 -0200

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,6 @@ mongoose.Promise = global.Promise;
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost:27017/Todos');
+mongoose.connect(process.env.MLAB_URI);
 
 module.exports = { mongoose };
